3f2ea637e8f43ebfe8efd3a82efe4d4f022f704c
israelMouofo <israel.mouofo@afscor.com>
2022-02-09 23:40:40 +0100

    add  one to many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_145752) do
+ActiveRecord::Schema.define(version: 2022_02_03_095455) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_02_02_145752) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "quantity"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
