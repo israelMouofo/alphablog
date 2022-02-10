818cf1530d10a1edc583c0a2be6236e69872f781
israelMouofo <israel.mouofo@afscor.com>
2022-02-10 11:41:19 +0100

    add  user edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
 
+  before_action :set_user
+
   def new
     @user = User.new
   end
+
+  def edit
+
+  end
+
+  def update
+   if @user.update(user_params)
+    flash[:notice] = "Your acoount was updated successfully"
+    redirect_to articles_path
+    else
+      render 'edit'
+   end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -17,5 +33,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
