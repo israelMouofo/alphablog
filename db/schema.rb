b382b62eceabc47ca881d8afe89fcf694b3536ee
israelMouofo <israel.mouofo@afscor.com>
2022-02-10 11:12:22 +0100

    add  user create action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_03_095455) do
+ActiveRecord::Schema.define(version: 2022_02_09_230417) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_02_03_095455) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
